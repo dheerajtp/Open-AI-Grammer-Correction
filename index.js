83acf7e0dc70e97e283bca8c1a7351886505beaf
Dheeraj TP <45311336+dheerajtp@users.noreply.github.com>
2023-02-02 08:42:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import cors from "cors";
 
 import route from "./route/";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
 route(app);
 
 app.listen(PORT, () => {
